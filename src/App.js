7d6fe623369e69fa73348b343c5d31f0e247fce6
Sami Barghshoon <sdbar@Samis-MBP.verizon.net>
2019-08-19 14:35:09 -0400

    -updates to stylesheet

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@
 */
 
 import React, { Component } from 'react';
-import NavigationBar from './components/NavigationBar'
-import CarouselBanner from './components/CarouselBanner'
-import PostContainer from './components/PostContainer'
-import Footer from './components/Footer'
+import NavigationBar from './components/NavigationBar';
+import CarouselBanner from './components/CarouselBanner';
+import PostContainer from './components/PostContainer';
+import Footer from './components/Footer';
 import './styles/app.css';
 
 class App extends Component {
